27ed27d7154cb6dc2a2b2d4cdae43ebd3000eab3
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-08-26 19:53:52 +0300

    Add files via upload

---
+++ main.php
@@ -0,0 +1,18 @@
+<?php
+/******************************************************************************
+
+    Emre Üçbudak Karadeniz Teknik Üniversitesi
+
+*******************************************************************************/
+
+$dizi = array(
+    "isim" => "emre",
+    "soyisim" => "Üçbudak",
+    "yil" => 2050);
+    
+echo($dizi["isim"]);
+$uyeler = array();
+$uyeler["isim"] = "AYŞE";
+$uyeler["yil"] = 2050;
+echo($uyeler["isim"])
+?>
